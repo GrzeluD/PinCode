2220d15b04b424b9fd43a64946a9ecfaa639fe28
GrzeluD <grzegorzdziedzic@onet.pl>
2021-11-21 14:25:12 +0100

    if/else statements refacored

--- pinCode.rb
+++ pinCode.rb
@@ -13,66 +13,26 @@ class PinCode
       when "U"
         if digit == 1 || digit == 2 || digit == 3
           digit
-        elsif digit == 4
-          digit = 1
-        elsif digit == 5
-          digit = 2
-        elsif digit == 6
-          digit = 3
-        elsif digit == 7
-          digit = 4
-        elsif digit == 8
-          digit = 5
-        elsif digit == 9
-          digit = 6
+        else
+          digit -= 3
         end
       when "R"
         if digit == 3 || digit == 6 || digit == 9
           digit
-        elsif digit == 1
-          digit = 2
-        elsif digit == 4
-          digit = 5
-        elsif digit == 7
-          digit = 8
-        elsif digit == 2
-          digit = 3
-        elsif digit == 5
-          digit = 6
-        elsif digit == 8
-          digit = 9
+        else
+          digit += 1
         end
       when "D"
         if digit == 7 || digit == 8 || digit == 9
           digit
-        elsif digit == 1
-          digit = 4
-        elsif digit == 2
-          digit = 5
-        elsif digit == 3
-          digit = 6
-        elsif digit == 4
-          digit = 7
-        elsif digit == 5
-          digit = 8
-        elsif digit == 6
-          digit = 9
+        else
+          digit += 3
         end
       when "L"
         if digit == 1 || digit == 4 || digit == 7
           digit
-        elsif digit == 2
-          digit = 1
-        elsif digit == 3
-          digit = 2
-        elsif digit == 5
-          digit = 4
-        elsif digit == 6
-          digit = 5
-        elsif digit == 8
-          digit = 7
-        elsif digit == 9
-          digit = 8
+        else
+          digit -= 1
         end
       else
       end
